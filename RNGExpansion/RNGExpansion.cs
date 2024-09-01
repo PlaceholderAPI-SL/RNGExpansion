d547609ee93bbe2f469150e96b3023ad83dff60a
NotZer0Two <lyonfan2010@gmail.com>
2024-09-01 09:01:30 +0200

    Aggiungere i file di progetto.

---
+++ RNGExpansion/RNGExpansion.cs
@@ -0,0 +1,36 @@
+﻿using PlaceholderAPI.API.Abstract;
+using Exiled.API.Features;
+using Exiled.API.Extensions;
+
+namespace RNGExpansion
+{
+    public class RNGExpansion : PlaceholderExpansion
+    {
+        public override string Author { get; set; } = "NotZer0Two";
+        public override string Identifier { get; set; } = "rng";
+        public override string RequiredPlugin { get; set; } = null;
+
+        public override string OnOfflineRequest(string param)
+        {
+            string converted = PlaceholderAPI.API.PlaceholderAPI.SetBracketsPlaceholders(param);
+            string[] split = converted.Split('_');
+            
+            switch(split[0].ToLower())
+            {
+                case "online":
+                    return Player.List.GetRandomValue().Nickname;
+                case "random":
+                    return UnityEngine.Random.Range(float.MinValue, float.MaxValue).ToString();
+                case "color":
+                    return UnityEngine.Random.ColorHSV().ToHex();
+            };
+
+            if (split.Length == 2 && int.TryParse(split[0], out int min) && int.TryParse(split[1], out int max))
+            {
+                return UnityEngine.Random.Range(min, max + 1).ToString();
+            }
+
+            return null;
+        }
+    }
+}
